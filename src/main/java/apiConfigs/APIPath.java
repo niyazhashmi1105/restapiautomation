c7e16b958b1bda3b642c11fde015e9925b6a9746
unknown <niyaz.hashmi@asians.cloud>
2023-01-25 12:35:03 +0530

    REST API Automation

---
+++ src/main/java/apiConfigs/APIPath.java
@@ -0,0 +1,19 @@
+package apiConfigs;
+
+public class APIPath {
+	
+	public static final class apiPath{
+		
+		//GET
+		public static final String GET_LIST_OF_POSTS ="api/users/2/";
+		public static final String GET_SINGLE_POST="posts/";
+		
+		//POST
+		public static final String CREATE_POST="posts";
+		
+		
+		//update post 
+		public static final String UPDATE_POST="posts";
+	}
+	
+}
